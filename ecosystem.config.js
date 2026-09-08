d27950a4c6bf478687310b4900fa9cdf8071edd7
nux2333 <wangyuqian02@gmail.com>
2026-09-08 21:13:14 +0900

    Keep PM2 ecosystem startup server-only

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,29 +6,6 @@ module.exports = {
       cwd: __dirname,
       autorestart: true,
       restart_delay: 3000
-    },
-    {
-      name: 'superlike-scan',
-      script: 'src/superlike-scanner.js',
-      cwd: __dirname,
-      autorestart: true,
-      restart_delay: 5000
-    },
-    ...['1', '2', '3', '4'].map(mode => ({
-      name: 'superlike-mode' + mode,
-      script: 'scripts/recheck-superlike.js',
-      cwd: __dirname,
-      env: {
-        SUPERLIKE_RECHECK_MODE: mode
-      },
-      autorestart: true,
-      restart_delay: 5000
-    })),
-    {
-      name: 'weibo-proxy-pool',
-      script: 'scripts/build-weibo-proxy-pool.js',
-      cwd: __dirname,
-      autorestart: false
     }
   ]
 };
