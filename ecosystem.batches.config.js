43d4b37b5495499102066d55d82a55b62e7732e5
nux2333 <wangyuqian02@gmail.com>
2026-09-09 13:08:40 +0900

    Run only required SuperLike services continuously

--- ecosystem.batches.config.js
+++ ecosystem.batches.config.js
@@ -1,33 +1,52 @@
 module.exports = {
   apps: [
     {
+      name: 'weibo-server',
+      script: 'server.js',
+      cwd: __dirname,
+      interpreter: process.execPath,
+      windowsHide: true,
+      autorestart: true,
+      restart_delay: 3000
+    },
+    {
       name: 'superlike-scan',
-      script: 'src/superlike-scanner.js',
+      script: 'scripts/start-superlike-workers.js',
       cwd: __dirname,
       interpreter: process.execPath,
       windowsHide: true,
       autorestart: true,
       restart_delay: 5000
     },
-    ...['1', '2', '3', '4'].map(mode => ({
-      name: 'superlike-mode' + mode,
+    {
+      name: 'superlike-mode4',
       script: 'scripts/recheck-superlike.js',
       cwd: __dirname,
       interpreter: process.execPath,
       windowsHide: true,
       env: {
-        SUPERLIKE_RECHECK_MODE: mode
+        SUPERLIKE_RECHECK_MODE: '4'
       },
       autorestart: true,
       restart_delay: 5000
-    })),
+    },
+    {
+      name: 'superlike-jyz',
+      script: 'scripts/backfill-today-jyz.js',
+      cwd: __dirname,
+      interpreter: process.execPath,
+      windowsHide: true,
+      autorestart: true,
+      restart_delay: 5000
+    },
     {
       name: 'weibo-proxy-pool',
       script: 'scripts/build-weibo-proxy-pool.js',
       cwd: __dirname,
       interpreter: process.execPath,
       windowsHide: true,
-      autorestart: false
+      autorestart: true,
+      restart_delay: 5000
     }
   ]
 };
