5953fc4a5d07c1a5969c2d0f6f0b562ceeb701a4
nux2333 <wangyuqian02@gmail.com>
2026-09-11 06:11:38 +0900

    Run legacy PM2 server on PostgreSQL

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,16 @@
+const path = require('path');
+
 module.exports = {
   apps: [
     {
       name: 'weibo-server',
       script: 'server.js',
       cwd: __dirname,
+      interpreter: process.execPath,
+      node_args: [
+        '--require',
+        path.join(__dirname, 'src', 'postgres-preload.js')
+      ],
       autorestart: true,
       restart_delay: 3000
     }
