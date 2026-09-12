8953dd17fdc4860330664011d15f76107562178f
nux2333 <wangyuqian02@gmail.com>
2026-09-12 10:01:15 +0900

    fix: start mode4 through batch logger wrapper

--- ecosystem.batches.config.js
+++ ecosystem.batches.config.js
@@ -49,17 +49,7 @@ module.exports = {
       node_args: asyncModeNodeArgs,
       env: { ...common.env, SUPERLIKE_RECHECK_MODE: '3', PG_MODE3_POOL_MAX: '6', SUPERLIKE_MODE3_HTTP_CONCURRENCY: '8', SUPERLIKE_PROFILE_VERIFY_BATCH_SIZE: '300' }
     },
-    {
-      ...common,
-      name: 'superlike-mode4',
-      script: path.join(__dirname, 'scripts', 'recheck-superlike-mode4-http.js'),
-      env: {
-        ...common.env,
-        SUPERLIKE_RECHECK_MODE: '4',
-        SUPERLIKE_LIST_NIGHT_INTERVAL_MS: String(3 * 60 * 1000),
-        SUPERLIKE_LIST_BOUNDARY_SAFETY_MAX_PAGES: '150'
-      }
-    },
+    { ...common, name: 'superlike-mode4', script: path.join(__dirname, 'scripts', 'recheck-superlike-mode4.js'), env: { ...common.env, SUPERLIKE_RECHECK_MODE: '4', SUPERLIKE_LIST_NIGHT_INTERVAL_MS: String(3 * 60 * 1000), SUPERLIKE_LIST_BOUNDARY_SAFETY_MAX_PAGES: '150' } },
     { ...common, name: 'superlike-jyz', script: path.join(__dirname, 'scripts', 'backfill-today-jyz.js'), env: { ...common.env, PLAYWRIGHT_GOTO_HARD_TIMEOUT_MS: '10000', PLAYWRIGHT_EVALUATE_HARD_TIMEOUT_MS: '12000', JYZ_BACKFILL_PROXY_RETRIES: '2' } },
     { ...common, name: 'weibo-proxy-pool', script: path.join(__dirname, 'scripts', 'build-weibo-proxy-pool.js') }
   ]
